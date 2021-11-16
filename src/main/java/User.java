3cf236a50c42118b7ebfb17929c5eadac2cf82ac
Abril <abrilcarini14@gmail.com>
2021-11-16 00:12:32 -0300

    agregue la clase User

---
+++ src/main/java/User.java
@@ -0,0 +1,47 @@
+public class User {
+    private String nombre;
+    private String dni;
+    private String mail;
+    private String contrasenia;
+    private String direccion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
